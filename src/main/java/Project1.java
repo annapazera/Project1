d564aa8d9ef03b93488d15ba3bc5b10ab7434a00
Anna Pazera <anna.pazera@tlen.pl>
2016-07-31 19:12:14 +0200

    First push with class Project1

---
+++ src/main/java/Project1.java
@@ -0,0 +1,50 @@
+import project.shop.Product;
+
+/**
+ * Created by Anna Kacprzak on 2016-07-29.
+ */
+public class Project1 {
+    public static void main(String[] args) {
+//        Osoba osoba = new Osoba ("Ania", "Pazera", 35 );
+//
+//
+//        osoba.wyswietlDane();
+//        String dane = osoba.toString();
+//        System.out.println(dane);
+//
+//        Samochod samochod = new Samochod("Skoda" , "Fabia", 15, 1.4 );
+//
+//        System.out.println(samochod.toString());
+//        samochod.minalRok();
+//        System.out.println(samochod.toString());
+//       String  daneTech=samochod.makraModel();
+//        System.out.println("Mój samochód to " + daneTech);
+
+//        BMI bmi = new BMI ("Ania", "Pazera");
+//
+//        System.out.println(bmi.toString());
+//        bmi.wzrost = 175;
+//        bmi.waga = 73;
+//        System.out.println(bmi.toString());
+//
+//        System.out.println("Twoje BMI to " + bmi.obliczBMI() );
+
+//        Silnik silnik = new Silnik ();
+//        silnik.moc=60;
+//        silnik.momentObrotowy=50;
+//        silnik.pojemnosc=2.0;
+//
+//        System.out.println(silnik.toString());
+//
+//        SkrzyniaBiegów skrzynia = new SkrzyniaBiegów(true, 5) ;
+//        Kolo kolo = new Kolo(45, 15);
+//        System.out.println(kolo.toString());
+//
+//        kolo.ustawCisnienie (2.5);
+//        System.out.println(kolo.toString());
+
+        Product product=new Product("Sukienka wieczorowa", "Sukienki", 150, "Piękna sukienka na wesele");
+        System.out.println(product);
+
+    }
+}
