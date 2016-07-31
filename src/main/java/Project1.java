90ec5f4e44fd436a71d8d9e32940cf51d7094a63
Anna Pazera <anna.pazera@tlen.pl>
2016-07-31 22:44:03 +0200

    nie przekierowuje mnie na strone sklepu

--- src/main/java/Project1.java
+++ src/main/java/Project1.java
@@ -1,10 +1,42 @@
 import project.shop.Product;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
 /**
  * Created by Anna Kacprzak on 2016-07-29.
  */
 public class Project1 {
     public static void main(String[] args) {
+        Spark.staticFileLocation("/webfiles");
+        String port= System.getenv("PORT");
+        if (port !=null) {
+            int portInt = Integer.parseInt(port);
+
+            Spark.port(portInt);
+            Spark.get("/AniSklep", (request, response)->
+            {
+                String name = request.queryParams("name");
+                String category = request.queryParams("category");
+                String price = request.queryParams("price");
+                double pricex = Double.parseDouble(price);
+                String description = request.queryParams("description");
+
+                Product product=new Product(name,category,pricex,description );
+
+                Map<String, Object> model = new HashMap();
+                model.put("product", product.toString());
+
+
+
+                return new ModelAndView(model, "sklepik.ftl");
+            }, new FreeMarkerEngine()
+            );
+
 //        Osoba osoba = new Osoba ("Ania", "Pazera", 35 );
 //
 //
@@ -43,8 +75,15 @@ public class Project1 {
 //        kolo.ustawCisnienie (2.5);
 //        System.out.println(kolo.toString());
 
-        Product product=new Product("Sukienka wieczorowa", "Sukienki", 150, "Piękna sukienka na wesele");
-        System.out.println(product);
+//        Product product=new Product("Sukienka wieczorowa", "Sukienki", 150, "Piękna sukienka na wesele");
+//        System.out.println(product);
+
+//        Ksiazka ksiazka = new Ksiazka ("Thinking in JAVA", "Bruce Eckel");
+//        ksiazka.wydawnictwo="Helion";
+//        ksiazka.opis="To jest bardzo dobra książka dla uczących sie programować w Javie";
+//        ksiazka.iloscStron=1248;
+//        ksiazka.cena=100;
+//        ksiazka.pokazOpis();
 
     }
-}
+} }
