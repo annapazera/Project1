379f49314d681d90897baa4a89d638a2fa53969e
Anna Pazera <anna.pazera@tlen.pl>
2016-08-01 10:54:24 +0200

    ShoppingCart class added

---
+++ src/main/java/project/shop/ShoppingCart.java
@@ -0,0 +1,11 @@
+package project.shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Anna Kacprzak on 2016-07-31.
+ */
+public class ShoppingCart {
+    public List<Product> products = new ArrayList<Product>();
+}
