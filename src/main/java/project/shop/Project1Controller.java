ab4282967a6f3855b54f3a6d442234c6e0dde1ae
Anna Pazera <anna.pazera@tlen.pl>
2016-08-10 15:44:59 +0200

    dodana kupione.html ; przycisk KUP; przekierowanie na strone z zakupani

--- src/main/java/project/shop/Project1Controller.java
+++ src/main/java/project/shop/Project1Controller.java
@@ -4,38 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import spark.ModelAndView;
-import spark.Spark;
-import spark.template.freemarker.FreeMarkerEngine;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class Project1Controller {
 
-
+    private ArrayList<Product> listaProduktow = new ArrayList<Product>();
 
     @RequestMapping("/form01")
-    public String form(){
+    public String form() {
         return "form02";
     }
-
-    ArrayList<Product> listaProduktow = new ArrayList<Product>();
 
     @RequestMapping("/AniSklep")
     public String sklep(@RequestParam(value = "name", required = true) String name,
                         @RequestParam(value = "category", required = true) String category,
                         @RequestParam(value = "price", required = true) String price,
                         @RequestParam(value = "description", required = true) String description, Model model) throws ProduktJuzIstniejeException {
-
-
-//                    String name = request.queryParams("name");
-//                    String category = request.queryParams("category");
-//                    String price = request.queryParams("price");
-//                    double pricex = Double.parseDouble(price);
-//                    String description = request.queryParams("description");
 
         for (Product item : listaProduktow) {
             if (item.getName().equals(name)) {
@@ -50,13 +36,19 @@ public class Project1Controller {
 
         return "newSklepik";
 
+    }
 
-//                    Map<String, Object> model = new HashMap();
-//                    model.put("product", product.toString());
-//                    model.put("products", listaProduktow);
-//
-//                    return new ModelAndView(model, "sklepik.ftl");
-//                }, new FreeMarkerEngine()
-//        );
+    @RequestMapping("/kup")
+    public String kupowanie(@RequestParam(value = "name", required = true) String name, Model model) {
+        for (Product product : listaProduktow) {
+
+            if (name.equals(product.getName())) {
+
+                model.addAttribute("product", product);
+
+
+            }
+        }
+        return "kupione";
     }
 }
